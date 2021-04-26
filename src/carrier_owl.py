66a0f043938d583559c46c5d379ba2805b597f2a
tomato-rikkyo <67274074+tomato-rikkyo@users.noreply.github.com>
2021-04-26 18:34:23 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -180,7 +180,7 @@ def main():
 #                            max_results=1000,
 #                            sort_by='submittedDate',
 #                            iterative=False)
-    articles = arxiv.query(query = f'cat:cs.* AND abs:reinforcement submittedDate:{day_before_yesterday_str}000000 TO {today_str}235959',
+    articles = arxiv.query(query = f'cat:cs.* AND abs:"reinforcement learning" submittedDate:{day_before_yesterday_str}000000 TO {today_str}235959',
                             max_results = 1000,
                             sort_by = 'submittedDate')
     results = search_keyword(articles, keywords, score_threshold)
