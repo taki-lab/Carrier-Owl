2114d42965598dd59272bba108b76ab57ea2924d
tomato-rikkyo <67274074+tomato-rikkyo@users.noreply.github.com>
2021-07-03 17:52:54 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -167,7 +167,7 @@ def main():
     keywords = config['keywords']
     score_threshold = float(config['score_threshold'])
 
-    day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=9)
+    day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=8)
 #     today_str = datetime.datetime.today().strftime('%Y%m%d')
     day_before_yesterday_str = day_before_yesterday.strftime('%Y%m%d')
     # datetime format YYYYMMDDHHMMSS
