5ed297edba831ba5c03735a519394330a3ac97b1
tomato-rikkyo <67274074+tomato-rikkyo@users.noreply.github.com>
2021-11-04 14:17:07 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -180,7 +180,7 @@ def main():
 #                            max_results=1000,
 #                            sort_by='submittedDate',
 #                            iterative=False)
-    articles = arxiv.query(query = f'cat:cs.* AND abs:"reinforcement learning" AND submittedDate:[{day_before_yesterday_str}000000 TO {day_before_yesterday_str}235959]', sort_by = 'submittedDate')
+    articles = arxiv.query(query = f'cat:cs.* AND abs:"world model" AND submittedDate:[20000101000000 TO 20211103235959]', sort_by = 'submittedDate')
     results = search_keyword(articles, keywords, score_threshold)
 
     slack_id = os.getenv("SLACK_ID") or args.slack_id
